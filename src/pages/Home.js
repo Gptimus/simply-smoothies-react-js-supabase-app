297a4d2174a04105aeaceab566d7f06c9059d9c2
Guerth Manzala <guerthmanzala@gmail.com>
2023-08-05 09:11:01 +0100

    feat: filter after delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,12 @@ const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
 
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((smoothy) => smoothy.id !== id);
+    });
+  };
+
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase.from("smoothies").select();
@@ -33,7 +39,11 @@ const Home = () => {
           {/* order-by buttons */}
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
-              <SmoothieCard smoothie={smoothie} key={smoothie.id} />
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
